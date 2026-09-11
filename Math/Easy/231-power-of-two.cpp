fdd40bb6a0ab9db4d4c63e87b1c071979dc08717
divyanshsrivastava0359 <divyanshsrivastava0359@gmail.com>
2026-09-11 10:17:57 +0530

    Update: 231. Power of Two (C++)

--- Math/Easy/231-power-of-two.cpp
+++ Math/Easy/231-power-of-two.cpp
@@ -3,7 +3,7 @@
 // Difficulty: Easy
 // Topics: Math, Bit Manipulation, Recursion
 // Runtime: 0 ms (Beats 100.0%)
-// Memory: 7.7 MB (Beats 98.9%)
+// Memory: 7.9 MB (Beats 12.7%)
 // Submitted: Sep 11, 2026
 // Link: https://leetcode.com/problems/power-of-two/
 // ═══════════════════════════════════════════════════════
@@ -12,6 +12,7 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
         if(n<=0) return false;
+        if(1073741824%n==0)return true;
 
         while(n%2==0){
             n=n/2;
@@ -20,3 +21,5 @@ public:
         return false;
     }
 };
+
+
