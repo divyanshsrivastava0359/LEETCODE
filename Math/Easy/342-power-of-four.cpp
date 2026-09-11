2a432f4e06df7922474b4bb2666fe48e09b498c1
divyanshsrivastava0359 <divyanshsrivastava0359@gmail.com>
2026-09-11 10:30:10 +0530

    Update: 342. Power of Four (C++)

--- Math/Easy/342-power-of-four.cpp
+++ Math/Easy/342-power-of-four.cpp
@@ -3,20 +3,37 @@
 // Difficulty: Easy
 // Topics: Math, Bit Manipulation, Recursion
 // Runtime: 0 ms (Beats 100.0%)
-// Memory: 7.8 MB (Beats 48.6%)
+// Memory: 7.8 MB (Beats 80.5%)
 // Submitted: Sep 11, 2026
 // Link: https://leetcode.com/problems/power-of-four/
 // ═══════════════════════════════════════════════════════
 
-class Solution {
+/*class Solution {
 public:
     bool isPowerOfFour(int n) {
         if(n<=0)return false;
-
-        while(n%4==0){
+        return (1073741824 % n ==0 &&  (n & (n - 1)) == 0);
+        /*while(n%4==0){
             n=n/4;
         }
         if(n==1)return true;
         return false;
     }
+};*/
+
+/*class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if (n <= 0) return false;
+        return (1073741824 % n == 0 && (n & (n - 1)) == 0);
+    }
+};*/
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        // 3. Check karein ki 1 active bit sirf odd position par hai ya nahi
+        return n > 0 && (n & (n - 1)) == 0 && (n & 0x55555555) != 0;
+    }
 };
+
+
