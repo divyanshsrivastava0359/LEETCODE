5eaaa430d656f8fb2b24586d436645b07095b31b
divyanshsrivastava0359 <divyanshsrivastava0359@gmail.com>
2026-09-22 10:36:39 +0530

    Update: 100. Same Tree (C++)

--- Tree/Easy/100-same-tree.cpp
+++ Tree/Easy/100-same-tree.cpp
@@ -2,8 +2,8 @@
 // Problem: 100. Same Tree
 // Difficulty: Easy
 // Topics: Tree, Depth-First Search, Breadth-First Search, Binary Tree
-// Runtime: 3 ms (Beats 0.6%)
-// Memory: 13 MB (Beats 13.0%)
+// Runtime: 0 ms (Beats 100.0%)
+// Memory: 12.7 MB (Beats 96.6%)
 // Submitted: Sep 22, 2026
 // Link: https://leetcode.com/problems/same-tree/
 // ═══════════════════════════════════════════════════════
@@ -18,7 +18,7 @@
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
- */
+ 
 class Solution {
 public:
     vector<int>res;
@@ -60,4 +60,16 @@ public:
          }
          return false;
     }
+};
+*/
+class Solution {
+public:
+        bool isSameTree(TreeNode* p, TreeNode* q) {
+          if(p==nullptr && q==nullptr) return true;
+
+          if(p==nullptr || q==nullptr) return false;
+
+          return (p->val==q->val) && isSameTree(p->left,q->left) && isSameTree(p->right , q->right);
+
+        }
 };
