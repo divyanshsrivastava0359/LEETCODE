dfb8c1958a5f977072308bcd47a9b1bd5453dc09
divyanshsrivastava0359 <divyanshsrivastava0359@gmail.com>
2026-09-22 10:38:25 +0530

    Update: 100. Same Tree (C++)

--- Tree/Easy/100-same-tree.cpp
+++ Tree/Easy/100-same-tree.cpp
@@ -2,8 +2,8 @@
 // Problem: 100. Same Tree
 // Difficulty: Easy
 // Topics: Tree, Depth-First Search, Breadth-First Search, Binary Tree
-// Runtime: 0 ms (Beats 100.0%)
-// Memory: 12.7 MB (Beats 96.6%)
+// Runtime: 3 ms (Beats 0.6%)
+// Memory: 12.9 MB (Beats 13.0%)
 // Submitted: Sep 22, 2026
 // Link: https://leetcode.com/problems/same-tree/
 // ═══════════════════════════════════════════════════════
@@ -17,7 +17,7 @@
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ * };*/
  
 class Solution {
 public:
@@ -46,30 +46,22 @@ public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
          if(p==nullptr && q==nullptr) return true;
          
-         res.clear();
-        kit.clear();
+         if(p==nullptr || q==nullptr) return false;
          preorder(p);
          preeorder(q);
-         if(res.size()==kit.size()){
-            for(int i=0;i<res.size();i++){
-                if(res[i]!=kit[i]){
-                    return false;
-                }
-            }
-            return true;
-         }
-         return false;
+         
+         return res==kit;
     }
 };
-*/
-class Solution {
-public:
-        bool isSameTree(TreeNode* p, TreeNode* q) {
-          if(p==nullptr && q==nullptr) return true;
 
-          if(p==nullptr || q==nullptr) return false;
+// /*class Solution {
+// public:
+//         bool isSameTree(TreeNode* p, TreeNode* q) {
+//           if(p==nullptr && q==nullptr) return true;
 
-          return (p->val==q->val) && isSameTree(p->left,q->left) && isSameTree(p->right , q->right);
+//           if(p==nullptr || q==nullptr) return false;
 
-        }
-};
+//           return (p->val==q->val) && isSameTree(p->left,q->left) && isSameTree(p->right , q->right);
+
+//         }
+// };*/
